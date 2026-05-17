2195d8558a47749658f9afc6c589cf808e7a6d8f
VICTOR PONTUAL GUEDES ARRUDA NOBREGA <victorpontualgan@gmail.com>
2026-05-17 18:53:25 -0300

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from src.database import engine
-from sqlalchemy import text
-
-with engine.connect() as conn:
-    result = conn.execute(text('SELECT 1'))
-    print('Conexao com Supabase OK:', result.fetchone())
