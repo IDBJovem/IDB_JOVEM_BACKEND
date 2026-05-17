d8c4b441cf927fa2bc0f4b716786de5e6014ad24
VICTOR PONTUAL GUEDES ARRUDA NOBREGA <victorpontualgan@gmail.com>
2026-05-17 18:41:13 -0300

    feat: criar model do produto

---
+++ src/produto/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, Text
+from src.database import Base
+
+
+class Produto(Base):
+    __tablename__ = "produto"
+
+    produto_id   = Column(Integer, primary_key=True, autoincrement=True)
+    nome         = Column(Text, nullable=False)
+    descricao    = Column(Text, nullable=True)
+    link_produto = Column(Text, nullable=True)
