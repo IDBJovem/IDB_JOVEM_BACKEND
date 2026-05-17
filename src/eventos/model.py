0880345a0dc2062341c4610c2e2ac3debbe5fc2e
Leticia-Arisa-K-Higa <higaleticia11@gmail.com>
2026-05-17 18:12:03 -0300

    refactor: reorganiza estrutura de pastas

--- src/eventos/model.py
+++ src/eventos/model.py
@@ -1,16 +1,15 @@
 # É aqui que definimos a tabela física do banco de dados para Eventos.
 # O código é meramente ilustrativo e deve ser ajustado de acordo com o nosso projeto.
 
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from src.database import Base
 
-class ModeloEvento(Base):
-    # Modelo para a tabela de eventos no banco de dados.
 
+class Evento(Base):
     __tablename__ = "eventos"
 
     id = Column(Integer, primary_key=True, index=True)
     titulo = Column(String(255), nullable=False)
-    data_inicio = Column(String(255), nullable=False)
-    data_fim = Column(String(255), nullable=False)
+    data_inicio = Column(DateTime, nullable=False)
+    data_fim = Column(DateTime, nullable=False)
     link_calendario = Column(Text, nullable=True)
