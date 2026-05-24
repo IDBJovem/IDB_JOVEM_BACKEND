266726e01612b6f146b50435875504a9ca8b3dde
Leticia-Arisa-K-Higa <higaleticia11@gmail.com>
2026-05-23 22:48:46 -0300

    Delete: Deleta os arquivos inúteis

--- src/main.py
+++ src/main.py
@@ -6,6 +6,11 @@ from src.banda_palestrante.controller import router as banda_palestrante_router
 from src.formulario.controller import router as formulario_router
 from src.produto.controller import router as produto_router
 from src.voluntario.controller import router as voluntario_router
+from src.auth.controller import router as auth_routers
+from src.evento.controller import router as evento_routers
+from src.atividade.controller import router as atividade_routers
+from src.drive.controller import router as drive_routers
+from src.mapa.controller import router as mapa_routers
 from src.config import configuracoes
 
 app = FastAPI(title="IDB Jovem Backend")
@@ -17,6 +22,11 @@ app.include_router(banda_palestrante_router)
 app.include_router(formulario_router)
 app.include_router(produto_router)
 app.include_router(voluntario_router)
+app.include_router(auth_routers)
+app.include_router(evento_routers)
+app.include_router(atividade_routers)
+app.include_router(drive_routers)
+app.include_router(mapa_routers)
 
 @app.get("/")
 def read_root():
